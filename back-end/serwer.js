49d1d4ef8ef947917035bd051f2c34aed7f63b48
Arcadius-WD <arkadiusz.kamyczek@gmail.com>
2024-01-02 14:25:28 +0100

    change

--- back-end/serwer.js
+++ back-end/serwer.js
@@ -46,10 +46,8 @@ app.patch('/todos/:todoId', (req, res) => {
 });
 
 
-const port = process.env.PORT || 8888;
-
-app.listen(port, ()=>{
-    console.log('Aplikacja wystartowała na porcie ${port}');
+app.listen(8888, ()=>{
+    console.log('Aplikacja wystartowała na porcie 8888');
 })
 
 
